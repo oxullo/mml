d70ccca2081df00568978a9e171c6e09712a35c8
OXullo Intersecans <x@brainrapers.org>
2021-10-23 02:00:53 +0200

    Adapt VEML7700 test for MML

--- experiments/veml7700/src/main.cpp
+++ experiments/veml7700/src/main.cpp
@@ -4,35 +4,35 @@
 Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
 void setup() {
-  while (!Serial) { delay(10); }
-  Serial.begin(115200);
-  Serial.println("Adafruit VEML7700 Test");
+  while (!SerialUSB) { delay(10); }
+  SerialUSB.begin(115200);
+  SerialUSB.println("Adafruit VEML7700 Test");
 
   if (!veml.begin()) {
-    Serial.println("Sensor not found");
+    SerialUSB.println("Sensor not found");
     while (1);
   }
-  Serial.println("Sensor found");
+  SerialUSB.println("Sensor found");
 
   veml.setGain(VEML7700_GAIN_1);
   veml.setIntegrationTime(VEML7700_IT_800MS);
 
-  Serial.print(F("Gain: "));
+  SerialUSB.print(F("Gain: "));
   switch (veml.getGain()) {
-    case VEML7700_GAIN_1: Serial.println("1"); break;
-    case VEML7700_GAIN_2: Serial.println("2"); break;
-    case VEML7700_GAIN_1_4: Serial.println("1/4"); break;
-    case VEML7700_GAIN_1_8: Serial.println("1/8"); break;
+    case VEML7700_GAIN_1: SerialUSB.println("1"); break;
+    case VEML7700_GAIN_2: SerialUSB.println("2"); break;
+    case VEML7700_GAIN_1_4: SerialUSB.println("1/4"); break;
+    case VEML7700_GAIN_1_8: SerialUSB.println("1/8"); break;
   }
 
-  Serial.print(F("Integration Time (ms): "));
+  SerialUSB.print(F("Integration Time (ms): "));
   switch (veml.getIntegrationTime()) {
-    case VEML7700_IT_25MS: Serial.println("25"); break;
-    case VEML7700_IT_50MS: Serial.println("50"); break;
-    case VEML7700_IT_100MS: Serial.println("100"); break;
-    case VEML7700_IT_200MS: Serial.println("200"); break;
-    case VEML7700_IT_400MS: Serial.println("400"); break;
-    case VEML7700_IT_800MS: Serial.println("800"); break;
+    case VEML7700_IT_25MS: SerialUSB.println("25"); break;
+    case VEML7700_IT_50MS: SerialUSB.println("50"); break;
+    case VEML7700_IT_100MS: SerialUSB.println("100"); break;
+    case VEML7700_IT_200MS: SerialUSB.println("200"); break;
+    case VEML7700_IT_400MS: SerialUSB.println("400"); break;
+    case VEML7700_IT_800MS: SerialUSB.println("800"); break;
   }
 
   //veml.powerSaveEnable(true);
@@ -44,16 +44,16 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Lux: "); Serial.println(veml.readLux());
-  Serial.print("White: "); Serial.println(veml.readWhite());
-  Serial.print("Raw ALS: "); Serial.println(veml.readALS());
+  SerialUSB.print("Lux: "); SerialUSB.println(veml.readLux());
+  SerialUSB.print("White: "); SerialUSB.println(veml.readWhite());
+  SerialUSB.print("Raw ALS: "); SerialUSB.println(veml.readALS());
 
   uint16_t irq = veml.interruptStatus();
   if (irq & VEML7700_INTERRUPT_LOW) {
-    Serial.println("** Low threshold");
+    SerialUSB.println("** Low threshold");
   }
   if (irq & VEML7700_INTERRUPT_HIGH) {
-    Serial.println("** High threshold");
+    SerialUSB.println("** High threshold");
   }
   delay(500);
 }
