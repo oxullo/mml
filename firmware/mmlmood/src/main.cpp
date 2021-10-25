96ee00503bf90debbb8637fbf0d2602da0c0ea6d
OXullo Intersecans <x@brainrapers.org>
2021-10-25 22:41:04 +0200

    Integrate LIS2DTW

--- firmware/mmlmood/src/main.cpp
+++ firmware/mmlmood/src/main.cpp
@@ -1,14 +1,38 @@
 #include <Arduino.h>
 
+#include "LIS2DTW12.h"
+
+LIS2DTW12 accel;
+
+
+void blink()
+{
+    static uint32_t ts_lastblink = 0;
+
+    if (millis() - ts_lastblink > 200) {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        ts_lastblink = millis();
+    }
+}
+
 void setup()
 {
+    Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
+    accel.begin();
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(20);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
+    if (accel.is_data_ready()) {
+        accel.read();
+
+        Serial.print(accel.mg().x);
+        Serial.print(" ");
+        Serial.print(accel.mg().y);
+        Serial.print(" ");
+        Serial.println(accel.mg().z);
+
+        blink();
+    }
 }
